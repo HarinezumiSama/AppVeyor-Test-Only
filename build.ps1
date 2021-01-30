8d714bc572c9a6f5701c6c87b21ee9c0236a4be8
Vitalii Maklai <vitalii.maklai@gmail.com>
2021-01-30 00:18:03 -0500

    Testing Git attribute: core.autocrlf=false

---
+++ build.ps1
@@ -0,0 +1,83 @@
+﻿#Requires -Version 5
+
+using namespace System
+using namespace System.Management.Automation
+
+[CmdletBinding(PositionalBinding = $false)]
+param ()
+begin
+{
+    $Script:ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+    Microsoft.PowerShell.Core\Set-StrictMode -Version 1
+
+    function Get-ErrorDetails([ValidateNotNull()] [System.Management.Automation.ErrorRecord] $errorRecord = $_)
+    {
+        [ValidateNotNull()] [System.Exception] $exception = $errorRecord.Exception
+        while ($exception -is [System.Management.Automation.RuntimeException] -and $exception.InnerException -ne $null)
+        {
+            $exception = $exception.InnerException
+        }
+
+        [string[]] $lines = `
+        @(
+            $exception.Message,
+            '',
+            '<<<',
+            "Exception: '$($exception.GetType().FullName)'",
+            "FullyQualifiedErrorId: '$($errorRecord.FullyQualifiedErrorId)'"
+        )
+
+        if (![string]::IsNullOrWhiteSpace($errorRecord.ScriptStackTrace))
+        {
+            $lines += `
+            @(
+                '',
+                'Script stack trace:',
+                '-------------------',
+                $($errorRecord.ScriptStackTrace)
+            )
+        }
+
+        if (![string]::IsNullOrWhiteSpace($exception.StackTrace))
+        {
+            $lines += `
+            @(
+                '',
+                'Exception stack trace:',
+                '----------------------',
+                $($exception.StackTrace)
+            )
+        }
+
+        $lines += '>>>'
+
+        return ($lines -join ([System.Environment]::NewLine))
+    }
+
+}
+process
+{
+    [Console]::ResetColor()
+    Write-Host ''
+
+    try
+    {
+        & git config --list --show-origin --show-scope
+
+        Write-Host ('-' * 100)
+
+        [string] $s = Get-Content -Raw -Path ./winFile.txt
+        $s | ConvertTo-Json -Depth 4 | Out-Host
+    }
+    catch
+    {
+        [string] $errorDetails = Get-ErrorDetails
+
+        [Console]::ResetColor()
+        Write-Host ''
+        Write-Host -ForegroundColor Red $errorDetails
+        Write-Host ''
+
+        throw
+    }
+}
